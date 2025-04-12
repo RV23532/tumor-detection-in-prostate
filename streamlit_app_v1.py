6243dc011e2414a62feda4f0f488ffd3f7661d78
RV23532 <m23air532@iitj.ac.in>
2025-04-12 11:16:08 +0530

    Backup

---
+++ streamlit_app_v1.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import cv2
+import numpy as np
+from PIL import Image
+import requests
+
+st.write("Tumor Detection in Prostrate 👁️") 
+
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+if uploaded_file:
+    image = Image.open(uploaded_file)
+else:
+    image = Image.open("images/default_image.png")  # Use a default image from the 'images' folder
+
+edges = cv2.Canny(np.array(image), 100, 200)
+tab1, tab2 = st.tabs(["Detected edges", "Original"])
+tab1.image(edges, use_column_width=True)
+tab2.image(image, use_column_width=True)
